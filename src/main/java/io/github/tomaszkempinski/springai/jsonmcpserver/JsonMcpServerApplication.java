504306e1afe910c2c9bce08263d849cee25343e7
tkempinski <tomasz.kempinski91@gmail.com>
2026-01-13 13:27:52 +0100

    Initial project structure and setup

---
+++ src/main/java/io/github/tomaszkempinski/springai/jsonmcpserver/JsonMcpServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.tomaszkempinski.springai.jsonmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonMcpServerApplication.class, args);
+	}
+
+}
